0428a462de17446be04e5bf8c9a1e8325b1517cf
guldfisk <ce.guldfisk@gmail.com>
2019-08-21 10:18:16 +0200

    tak

---
+++ api/management/commands/populatecubes.py
@@ -0,0 +1,71 @@
+import os
+import hashlib
+
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+
+from mocknames.generate import NameGenerator
+
+from mtgorp.models.serilization.strategies.jsonid import JsonId
+
+from misccube.cubeload.load import CubeLoader
+from misccube.trapification.algorithm import ConstrainedNodes
+from misccube.trapification.fetch import ConstrainedNodeFetcher
+
+from resources.staticdb import db
+from api import models
+
+
+class Command(BaseCommand):
+    help = 'Populate cubes'
+
+    def handle(self, *args, **options):
+        get_user_model().objects.all().delete()
+        models.VersionedCube.objects.all().delete()
+        models.CubeRelease.objects.all().delete()
+
+        root_user = get_user_model().objects.create_user(
+            username='root',
+            password=os.environ['ROOT_USER_PASSWORD'],
+            email='ceguldfisk@gmail.com',
+            is_staff=True,
+            is_superuser=True,
+        )
+        versioned_cube = models.VersionedCube.objects.create(
+            name='xd',
+            description='haha',
+            author=root_user,
+        )
+
+        name_generator = NameGenerator()
+
+        cube_loader = CubeLoader(db)
+
+        for cube, time in cube_loader.all_cubes():
+            models.CubeRelease.objects.create(
+                cube_content=JsonId.serialize(cube),
+                checksum=cube.persistent_hash(),
+                name=name_generator.get_name(
+                    int(
+                        hashlib.sha1(
+                            cube.persistent_hash().encode('ASCII')
+                        ).hexdigest(),
+                        16,
+                    )
+                ),
+                created_at=time,
+                versioned_cube=versioned_cube,
+                intended_size=360,
+            )
+
+        for cube in models.CubeRelease.objects.all():
+            print(cube.name, cube.checksum)
+
+        models.ConstrainedNodes.objects.create(
+            release=models.CubeRelease.objects.order_by('-created_at').first(),
+            constrained_nodes_content=JsonId.serialize(
+                ConstrainedNodes(
+                    ConstrainedNodeFetcher(db).fetch_garbage()
+                )
+            )
+        )
---
+++ api/management/commands/renderlapimages.py
@@ -0,0 +1,43 @@
+import itertools
+import time
+
+import promise
+
+from django.core.management.base import BaseCommand
+
+from mtgimg import interface
+
+from resources.staticimageloader import image_loader
+
+from api.models import CubeRelease
+
+
+class Command(BaseCommand):
+    help = 'Populate cubes'
+
+    def handle(self, *args, **options):
+
+        laps = set(
+            itertools.chain(
+                *(
+                    cube_container.cube.laps
+                    for cube_container in
+                    CubeRelease.objects.all()
+                )
+            )
+        )
+
+        st = time.time()
+
+        images = promise.Promise.all(
+            [
+                image_loader.get_image(trap, size_slug=size_slug, save=True)
+                for trap in
+                laps
+                for size_slug in
+                interface.SizeSlug
+            ]
+        ).get()
+
+        print(f'Done, rendered {len(images)} images.')
+        print(time.time() - st)
