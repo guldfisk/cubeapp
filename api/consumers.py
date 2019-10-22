2ce895b9bf4abfd9795ec3a8a078d556bfd8571e
guldfisk <ce.guldfisk@gmail.com>
2019-10-22 10:33:18 +0200

    aha

--- api/consumers.py
+++ api/consumers.py
@@ -171,15 +171,15 @@ class DistributorConsumer(AuthenticatedConsumer):
         self.accept()
 
     def disconnect(self, code):
-        if self._distribution_task is not None:
-            self._distribution_task.unsubscribe(
-                str(
-                    id(
-                        self
-                    )
-                )
-            )
-            self._consumer.stop()
+        # if self._distribution_task is not None:
+        #     self._distribution_task.unsubscribe(
+        #         str(
+        #             id(
+        #                 self
+        #             )
+        #         )
+        #     )
+        #     self._consumer.stop()
         if self._group_name is not None:
             async_to_sync(self.channel_layer.group_discard)(
                 self._group_name,
