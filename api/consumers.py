51e9777bbe2fe81d5d577acb167d668ebdb01803
guldfisk <ce.guldfisk@gmail.com>
2019-10-22 13:32:27 +0200

    aha

--- api/consumers.py
+++ api/consumers.py
@@ -656,15 +656,15 @@ class PatchEditConsumer(AuthenticatedConsumer):
                     'user': 'some user',
                 },
             )
-            if self._token is not None:
-                async_to_sync(self.channel_layer.group_send)(
-                    self._group_name,
-                    {
-                        'type': 'user_update',
-                        'action': 'leave',
-                        'user': self.scope['user'].username,
-                    },
-                )
+            # if self._token is not None:
+            #     async_to_sync(self.channel_layer.group_send)(
+            #         self._group_name,
+            #         {
+            #             'type': 'user_update',
+            #             'action': 'leave',
+            #             'user': self.scope['user'].username,
+            #         },
+            #     )
             async_to_sync(self.channel_layer.group_discard)(
                 self._group_name,
                 self.channel_name,
