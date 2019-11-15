6a302d87ac4e5c390281045f655de8020adf1712
carl-emil hjort-trærup <ce.guldfisk@gmail.com>
2019-11-15 16:34:34 +0100

    aha

--- frontend/src/components/images.tsx
+++ frontend/src/components/images.tsx
@@ -6,6 +6,13 @@ import {get_cardback_image_url, get_cubeable_images_url} from "./utils/utils";
 import {Cubeable} from "./models/models";
 
 
+const imageSizeMap: {[key: string]: [number, number]} = {
+  'original': [745, 1040],
+  'medium': [372, 520],
+  'small': [223, 312],
+  'thumbnail': [111, 156],
+};
+
 interface CubeableImageProps {
   cubeable?: Cubeable
   sizeSlug?: string
@@ -17,6 +24,7 @@ interface CubeableImageProps {
 export const CubeableImage: React.FunctionComponent<CubeableImageProps> = (
   {cubeable = null, sizeSlug = 'medium', onClick = null, id=null, type=null}: CubeableImageProps
 ) => {
+  const [width, height]: [number, number] = imageSizeMap[sizeSlug];
   return <LazyImage
     src={
       get_cubeable_images_url(
@@ -31,6 +39,8 @@ export const CubeableImage: React.FunctionComponent<CubeableImageProps> = (
         ref={ref}
         src={get_cardback_image_url(sizeSlug)}
         alt={imageProps.alt}
+        width={width}
+        height={height}
       />
     )}
     actual={({imageProps}) => <img {...imageProps} />}
